982a3b4377b60d073270b9c52e7e05a1741b329c
Kong Chang <kcha246@wgu.edu>
2023-05-17 21:52:52 -0700

    Add Appointment GUI Screen - Completed
    Modify Appointment GUI Screen - Completed
    
    Added all add and modify functions to the Appointment controller.
    
    Started on the Helper Class to help create help functions to reuse.

--- src/controller/Appointment.java
+++ src/controller/Appointment.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -10,5 +11,21 @@ public class Appointment implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Appointments are being loaded from database.");
+    }
+
+    /**  */
+    public void Add_Appointment_Button_Clicked(ActionEvent actionEvent) {
+    }
+
+    /**  */
+    public void Add_Appointment_Cancel_Button_Clicked(ActionEvent actionEvent) {
+    }
+
+    /**  */
+    public void Modify_Appointment_Button_Clicked(ActionEvent actionEvent) {
+    }
+
+    /**  */
+    public void Modify_Appointment_Cancel_Button_Clicked(ActionEvent actionEvent) {
     }
 }
---
+++ src/helper/helper.java
@@ -0,0 +1,14 @@
+package helper;
+
+/** This class is to contain all the helper functions so that we can reuse same functions throughout the program */
+public class helper {
+
+    /** This function is to make a alert but takes in the message and title so that it can resued */
+    public void create_alert(String inputMessage, String title ){
+
+    }
+
+
+
+
+}
